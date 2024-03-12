d7c6ca7e1713019d73db31ea26ad22a2d75f3b4d
Christian Loving <christian70401@gmail.com>
2024-03-11 23:27:22 -0500

    :test: alpha vantage class

---
+++ tests/alpha_connector/test_alpha_connector.py
@@ -0,0 +1,25 @@
+import os
+
+import dotenv
+import pytest
+
+dotenv.load_dotenv()
+
+API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+
+from alpha_connector.alpha_connector import AlphaVantage
+
+
+@pytest.mark.parametrize(
+    ("function", "interval", "symbol"),
+    [
+        ("TIME_SERIES_INTRADAY", "5min", "IBM"),
+        ("TIME_SERIES_DAILY", "1min", "AAPL"),
+        ("TIME_SERIES_WEEKLY", "15min", "GOOGL"),
+    ],
+)
+def test_get_data(function, interval, symbol):
+    """Test get_data method."""
+    av = AlphaVantage(API_KEY)
+    data = av.get_data(function, interval, symbol)
+    assert data is not None
